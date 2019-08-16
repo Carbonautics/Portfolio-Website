eaadde19af84a56d06ccd8d327c12b2856025ef6
Pratik Nadgouda <carbonautics@gmail.com>
2019-08-16 21:01:01 +0530

    navbar-fix
    Fixing the navbar.

--- JavaScript/js.js
+++ JavaScript/js.js
@@ -1,1 +1,18 @@
-
+//change active class
+$(document).ready(function () {
+    $('.navbar-nav li a').click(function(e) {
+        $('.navbar-nav li.active').removeClass('active');
+        var $parent = $(this).parent();
+        $parent.addClass('active');
+        e.preventDefault();
+    });
+});
+//change hover class
+$(document).ready(function () {
+    $('.navbar-nav li a').click(function(e) {
+        $('.navbar-nav li').addClass('hover');
+        var $parent = $(this).parent();
+        $parent.removeClass('hover');
+        e.preventDefault();
+    });
+});
