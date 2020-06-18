660bb382df98fee0b0475b8e013bd4c5e8480d70
Pratik Nadgouda <carbonautics@pm.me>
2020-06-18 15:15:20 +0530

    June 2020 Update

--- JavaScript/js.js
+++ JavaScript/js.js
@@ -1,18 +1,9 @@
 //change active class
 $(document).ready(function () {
-    $('.navbar-nav li a').click(function(e) {
-        $('.navbar-nav li.active').removeClass('active');
+    $('.navbarr-nav li a').click(function(e) {
+        $('.navbarr-nav li.active').removeClass('active');
         var $parent = $(this).parent();
         $parent.addClass('active');
         //e.preventDefault();
-    });
-});
-//change hover class
-$(document).ready(function () {
-    $('.navbar-nav li a').click(function(e) {
-        $('.navbar-nav li').addClass('hover');
-        var $parent = $(this).parent();
-        $parent.removeClass('hover');
-       // e.preventDefault();
     });
 });
